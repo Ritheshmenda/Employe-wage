35281103abd93e48cdfd6126aa4d8cfa246a542f
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-24 22:50:45 +0530

    [Rithesh]added:partime

--- Employe-wage/Employeewage.cs
+++ Employe-wage/Employeewage.cs
@@ -10,14 +10,22 @@ namespace employewage
         {
             Random random = new Random();
             int PERHRS = 20;
-            int workinghrs = 8;
-            int employeecheck = random.Next(0, 2);
+            int NO_OF_HOURS_FULL = 8;
+            int NO_OF_HOURS_PART = 4;
+            int employeecheck = random.Next(0, 3);
             if (employeecheck == 0)
-                Console.WriteLine("Employee is absent");
+            {
+                Console.WriteLine("employee is present full day");
+                Console.WriteLine("employe salary is" + PERHRS * NO_OF_HOURS_FULL);
+            }
+            else if (employeecheck == 1)
+            {
+                Console.WriteLine("employee is present for part time");
+                Console.WriteLine("employe salary is " + PERHRS * NO_OF_HOURS_PART);
+            }
             else
             {
-                Console.WriteLine("employee is present");
-                Console.WriteLine("employe salary" + PERHRS * workinghrs);
+                Console.WriteLine("Employee is absent");
             }
         }
     }
