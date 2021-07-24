619b289d9f65e47523443fe4d30e0daf1d2139e4
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-24 22:28:23 +0530

    [Rithesh]added:introduction

---
+++ Employe-wage/Employeewage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace employewage
+{
+    class Employeewage
+    {
+        public void Employewage()
+        {
+            Console.WriteLine("welcome to employe wage problem");
+        }
+    }
+}
---
+++ Employe-wage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace employewage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employeewage Employ = new Employeewage();
+            Employ.Employewage();
+        }
+    }
+}
