24360cd9acaa014067e42466362d0300f6ce8589
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-24 23:31:46 +0530

    [Rithesh]added:switchcase

--- Employe-wage/Employeewage.cs
+++ Employe-wage/Employeewage.cs
@@ -10,22 +10,22 @@ namespace employewage
         {
             Random random = new Random();
             int PERHRS = 20;
-            int NO_OF_HOURS_FULL = 8;
-            int NO_OF_HOURS_PART = 4;
+            int no_of_hours_full = 8;
+            int no_of_hours_part = 4;
             int employeecheck = random.Next(0, 3);
-            if (employeecheck == 0)
+            switch (employeecheck)
             {
-                Console.WriteLine("employee is present full day");
-                Console.WriteLine("employe salary is" + PERHRS * NO_OF_HOURS_FULL);
-            }
-            else if (employeecheck == 1)
-            {
-                Console.WriteLine("employee is present for part time");
-                Console.WriteLine("employe salary is " + PERHRS * NO_OF_HOURS_PART);
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
+                case 0:
+                    Console.WriteLine("employee is present full day");
+                    Console.WriteLine("employe salary is" + PERHRS * no_of_hours_full);
+                    break;
+                case 1:
+                    Console.WriteLine("employee is present for part time");
+                    Console.WriteLine("employe salary is " + PERHRS * no_of_hours_part);
+                    break;
+                case 2:
+                    Console.WriteLine("Employee is absent");
+                    break;
             }
         }
     }
