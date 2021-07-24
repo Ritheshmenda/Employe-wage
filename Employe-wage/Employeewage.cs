b7ef78c264c4e7a7a7d6f0f80856e4da3a03d536
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-24 22:38:36 +0530

    [Rithesh]added:salary

--- Employe-wage/Employeewage.cs
+++ Employe-wage/Employeewage.cs
@@ -9,11 +9,16 @@ namespace employewage
         public void Employewage()
         {
             Random random = new Random();
+            int PERHRS = 20;
+            int workinghrs = 8;
             int employeecheck = random.Next(0, 2);
             if (employeecheck == 0)
                 Console.WriteLine("Employee is absent");
             else
+            {
                 Console.WriteLine("employee is present");
+                Console.WriteLine("employe salary" + PERHRS * workinghrs);
+            }
         }
     }
 }
